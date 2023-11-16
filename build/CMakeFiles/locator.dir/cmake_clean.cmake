f8f77c5be18b0d192d6c00ba7c1d8935797f8a26
glebwinx <46938gt@rambler.ru>
2023-11-16 03:04:44 +0300

    Successful  build of  project locator version 0.1

---
+++ build/CMakeFiles/locator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/locator.dir/locator.cpp.o"
+  "CMakeFiles/locator.dir/locator.cpp.o.d"
+  "locator"
+  "locator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/locator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
